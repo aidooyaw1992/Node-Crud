f1c8d58f55cd4cca20367d4f5679455454c53cca
Daniel <aidooyaw1992@gmail.com>
2018-09-29 21:00:50 +0000

    changed agin to false

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));// for parsing application/x-www-form-urlencoded
 
 // app.use(multer({dest:'./uploads/', 
 //     rename: function(fieldname, filename){
